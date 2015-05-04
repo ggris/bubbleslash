14212ea802ade685f0d5c8acc58f69360dd1f8cb
Guillaume Gris <guillaume.gris_code@m4x.org>
2015-05-04 14:47:29 +0200

    corrected names and added test post shader

---
+++ BubbleSlash/Assets/scripts/CameraPostShader.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraPostShader : MonoBehaviour
+{
+	public Material post_material;
+	// Called by the camera to apply the image effect
+	void OnRenderImage (RenderTexture source, RenderTexture destination)
+	{
+		
+		//mat is the material containing your shader
+		Graphics.Blit (source, destination, post_material);
+	}
+
+}
