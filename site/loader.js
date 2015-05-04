445c4b45c75c2f413cf7e20aa2995a4ff003f6ea
Guillaume Gris <guillaume.gris_code@m4x.org>
2015-05-04 16:34:18 +0200

    updated site

---
+++ site/loader.js
@@ -0,0 +1,46 @@
+var config = {
+    width: 960, 
+    height: 600,
+    params: { enableDebugging:"0" }
+
+};
+var u = new UnityObject2(config);
+
+function loadGame() {
+
+    var version = window.location.search.substring(1);
+    document.getElementById("Version").innerHTML = version;
+    var $missingScreen = jQuery("#unityPlayer").find(".missing");
+    var $brokenScreen = jQuery("#unityPlayer").find(".broken");
+    $missingScreen.hide();
+    $brokenScreen.hide();
+
+    u.observeProgress(function (progress) {
+        switch(progress.pluginStatus) {
+            case "broken":
+                $brokenScreen.find("a").click(function (e) {
+                    e.stopPropagation();
+                    e.preventDefault();
+                    u.installPlugin();
+                    return false;
+                });
+                $brokenScreen.show();
+                break;
+            case "missing":
+                $missingScreen.find("a").click(function (e) {
+                    e.stopPropagation();
+                    e.preventDefault();
+                    u.installPlugin();
+                    return false;
+                });
+                $missingScreen.show();
+                break;
+            case "installed":
+                $missingScreen.remove();
+                break;
+            case "first":
+                break;
+        }
+    });
+    u.initPlugin(jQuery("#unityPlayer")[0], "game/bubbleslash-"+version+".unity3d");
+}
