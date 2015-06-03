414e833403bbed15764c39b955afa26a69d05539
Guillaume Gris <guillaume.gris_code@m4x.org>
2015-06-03 15:09:01 +0200

    added launcher

---
+++ BubbleSlash/Assets/scripts/LevelLoader.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelLoader : MonoBehaviour {
+
+	string level_;
+
+	// Use this for initialization
+	void Start () {
+		level_ = "TestLevel03";
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			Application.LoadLevel(level_);
+	
+	}
+
+}
