fcbe018f87036f133d8d3976e3407ecb65965819
totk <totorkepic@hotmail.fr>
2015-06-04 09:47:29 +0200

    wip new dodge hat

--- BubbleSlash/Assets/scripts/hatScripts/DodgeHatScript.cs
+++ BubbleSlash/Assets/scripts/hatScripts/DodgeHatScript.cs
@@ -2,30 +2,44 @@
 using System.Collections;
 
 public class DodgeHatScript : HatAbstractClass {
-	
-
+	public float dodge_duration = 0.3f;
+	public float speed = 2;
 	
 	
 	override public void onSpecialStateEnter(){
-	
 		timer = 0f;
-		player.GetComponent<Rigidbody2D> ().gravityScale = 0.3f;
-		player.GetComponent<Rigidbody2D> ().velocity = new Vector2(0,0);
-		player.GetComponent<PlayerPhysics> ().is_hitable = false;
+		setDodge ();
+		Invoke ("setFall", dodge_duration);
+
+
 
 	}
-	
+	void setDodge(){
+		Vector2 direction = player.GetComponent<PlayerPhysics> ().directionFromInput ();
+		player.GetComponent<Rigidbody2D> ().gravityScale = 0;
+		player.GetComponent<Rigidbody2D> ().velocity = direction*speed;
+		player.GetComponent<PlayerPhysics> ().is_hitable = false;
+	}
+
+	void setFall(){
+		player.GetComponent<Rigidbody2D> ().gravityScale = 0.3f;
+		player.GetComponent<Rigidbody2D> ().velocity = new Vector2(0,0);
+		player.GetComponent<PlayerPhysics> ().is_hitable = true;
+	}
+
+
 	override public void onSpecialStateExit(){
 		player.GetComponent<Rigidbody2D> ().gravityScale = 5;
 		player.GetComponent<Animator> ().SetTrigger ("stopHat");
-		player.GetComponent<Rigidbody2D> ().velocity = new Vector2(0,0);
-		player.GetComponent<PlayerPhysics> ().is_hitable = true;
+		//player.GetComponent<Rigidbody2D> ().velocity = new Vector2(0,0);
+		//player.GetComponent<PlayerPhysics> ().is_hitable = true;
 	}
 	override public bool hasSpecialState(){
 		return true;
 	}
 
 	override public void updateHat(){
+
 	}
 	override public void startHat(){
 		length = 1f;
