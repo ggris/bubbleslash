50222d49a877d70afe5a21702b831d2c6729488d
Guillaume Gris <guillaume.gris_code@m4x.org>
2015-06-04 10:20:24 +0200

    network progress

--- BubbleSlash/Assets/scripts/PlayerManager.cs
+++ BubbleSlash/Assets/scripts/PlayerManager.cs
@@ -2,97 +2,134 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class PlayerManager : MonoBehaviour {
+public class PlayerManager : MonoBehaviour
+{
 
 	public GameObject[] hat_prefabs;
 	public GameObject player_prefab;
-	public PlayerSettings.Hat [] hatChoices;
-
+	public PlayerSettings.Hat[] hatChoices;
 	public Transform[] spawn_points;
-	public GameObject [] players;
+	public GameObject[] players;
 	public float min_distance_spawn;
 	public float death_altitude;
-	private int [] score;
+	private int[] score;
 	private CameraTracking camera_tracking;
 
-
-	void Start () {
+	void Start ()
+	{
 		score = new int[players.Length];
 		camera_tracking = (CameraTracking)FindObjectOfType (typeof(CameraTracking));
 		spawn (0, spawn_points [0]);
 		spawn (1, spawn_points [1]);
 	}
 
-	void Update () {
+	void Update ()
+	{
 	}
-	public void dealWithDeath(int i){
+
+	public void dealWithDeath (int i)
+	{
 		score [i] -= 1;
-		Destroy(players [i]);
+		Destroy (players [i]);
 		StartCoroutine (spawnLater (i, players [i].transform.position, 0.5f));
 
 	}
-	public IEnumerator spawnLater(int i, Vector3 position_death, float delayTime){
-		yield return new WaitForSeconds(delayTime);
+
+	public IEnumerator spawnLater (int i, Vector3 position_death, float delayTime)
+	{
+		yield return new WaitForSeconds (delayTime);
 		Transform t = findRandomSpawnPoint (i, position_death);
 		spawn (i, t);
 	}
 
-
-	public void spawn(int i, Transform t){
+	public void spawn (int i, Transform t)
+	{
+		if (!Network.isClient && !Network.isServer) 
+			spawnOffline (i, t);
+		else
+			spawnOnline (i, t, 0);
+	}
+	
+	public void spawnOffline (int i, Transform t)
+	{
 		players [i] = GameObject.Instantiate (player_prefab, t.position, new Quaternion (0, 0, 0, 0)) as GameObject;
 		players [i].GetComponent<PlayerPhysics> ().playerNumber = i + 1;
 		players [i].gameObject.GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, 0);
 		SetSprites (i);
 		SetColor (i);
 		SetHatRendering (i);
-		GameObject hat_i = GameObject.Instantiate (hat_prefabs [(int)hatChoices[i]], new Vector3 (0, 0, 0), new Quaternion (0, 0, 0, 0)) as GameObject;
+		GameObject hat_i = GameObject.Instantiate (hat_prefabs [(int)hatChoices [i]], new Vector3 (0, 0, 0), new Quaternion (0, 0, 0, 0)) as GameObject;
 		hat_i.transform.parent = players [i].transform;
-		players [i].GetComponent<PlayerPhysics>().hat= hat_i;
+		players [i].GetComponent<PlayerPhysics> ().hat = hat_i;
 		camera_tracking.resetPlayers ();
 	}
 
-	Transform findRandomSpawnPoint(int i, Vector3 pos){
-		List<Transform> far_enough_spawn_points = new List<Transform>();
+	public void spawnOnline (int i, Transform t, int group)
+	{
+		players [i] = Network.Instantiate (player_prefab, t.position, new Quaternion (0, 0, 0, 0), group) as GameObject;
+		players [i].GetComponent<PlayerPhysics> ().playerNumber = i + 1;
+		players [i].gameObject.GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, 0);
+		SetSprites (i);
+		SetColor (i);
+		SetHatRendering (i);
+		GameObject hat_i = Network.Instantiate (hat_prefabs [(int)hatChoices [i]], new Vector3 (0, 0, 0), new Quaternion (0, 0, 0, 0), group) as GameObject;
+		hat_i.transform.parent = players [i].transform;
+		players [i].GetComponent<PlayerPhysics> ().hat = hat_i;
+		camera_tracking.resetPlayers ();
+		Debug.Log ("Spawn online");
+	}
+
+	Transform findRandomSpawnPoint (int i, Vector3 pos)
+	{
+		List<Transform> far_enough_spawn_points = new List<Transform> ();
 		foreach (Transform t in spawn_points) {
-			if ((t.position-pos).magnitude > min_distance_spawn)
-				far_enough_spawn_points.Add(t);
+			if ((t.position - pos).magnitude > min_distance_spawn)
+				far_enough_spawn_points.Add (t);
 		}
 		if (far_enough_spawn_points.Count > 0) 
 			return far_enough_spawn_points [Random.Range (0, far_enough_spawn_points.Count - 1)];
 		else 
-			return spawn_points[Random.Range(0,spawn_points.Length-1)];
+			return spawn_points [Random.Range (0, spawn_points.Length - 1)];
 	}
-	void SetSprites(int i){
-		Object [] sprites = Resources.LoadAll<Sprite>("sprites/playerSpriteSheet_"+"blue");
+
+	void SetSprites (int i)
+	{
+		Object [] sprites = Resources.LoadAll<Sprite> ("sprites/playerSpriteSheet_" + "blue");
 		Object weapon = Resources.Load<Sprite> ("sprites/playerSpriteSheet_sword");
 		players [i].transform.Find ("animation").Find ("body").gameObject.GetComponent<SpriteRenderer> ().sprite = (Sprite)sprites [0];
-		players [i].transform.Find ("animation").Find ("eye").gameObject.GetComponent<SpriteRenderer> ().sprite = (Sprite)sprites[1];
-		players [i].transform.Find ("animation").Find ("weapon_trans").gameObject.GetComponent<SpriteRenderer> ().sprite = (Sprite)sprites[2];
-		players [i].transform.Find ("animation").Find ("weapon_trans").Find ("weapon").gameObject.GetComponent<SpriteRenderer> ().sprite = (Sprite)sprites[3];
+		players [i].transform.Find ("animation").Find ("eye").gameObject.GetComponent<SpriteRenderer> ().sprite = (Sprite)sprites [1];
+		players [i].transform.Find ("animation").Find ("weapon_trans").gameObject.GetComponent<SpriteRenderer> ().sprite = (Sprite)sprites [2];
+		players [i].transform.Find ("animation").Find ("weapon_trans").Find ("weapon").gameObject.GetComponent<SpriteRenderer> ().sprite = (Sprite)sprites [3];
 	}
-	public Color [] colors;
-	void SetColor (int i){
+
+	public Color[] colors;
+
+	void SetColor (int i)
+	{
 		players [i].transform.Find ("animation").Find ("body").gameObject.GetComponent<SpriteRenderer> ().color = colors [i];
 		players [i].transform.Find ("animation").Find ("eye").gameObject.GetComponent<SpriteRenderer> ().color = colors [i];
 		players [i].transform.Find ("animation").Find ("weapon_trans").gameObject.GetComponent<SpriteRenderer> ().color = colors [i];
 	}
-	void SetHatRendering(int i){
-		PlayerSettings.Hat h = hatChoices[i];
+
+	void SetHatRendering (int i)
+	{
+		PlayerSettings.Hat h = hatChoices [i];
 		GameObject anim = players [i].transform.Find ("animation").gameObject;
 		if (h == PlayerSettings.Hat.dashHat) {
-			anim.transform.FindChild("dodgeHat").gameObject.SetActive(false);
-			anim.transform.FindChild("dashHat").gameObject.SetActive(true);
+			anim.transform.FindChild ("dodgeHat").gameObject.SetActive (false);
+			anim.transform.FindChild ("dashHat").gameObject.SetActive (true);
 		}
 		if (h == PlayerSettings.Hat.dodgeHat) {
-			anim.transform.FindChild("dashHat").gameObject.SetActive(false);
-			anim.transform.FindChild("dodgeHat").gameObject.SetActive(true);
+			anim.transform.FindChild ("dashHat").gameObject.SetActive (false);
+			anim.transform.FindChild ("dodgeHat").gameObject.SetActive (true);
 		}
 	}
 
-	void OnGUI(){
+	void OnGUI ()
+	{
 		for (int i=0; i <players.Length; i++) {
-			string message = "P" + i + " : " + score[i];
-			GUI.Box (new Rect (50*i, 10, 50, 25), message);
+			string message = "P" + i + " : " + score [i];
+			GUI.Box (new Rect (50 * i, 10, 50, 25), message);
 		}
 	}
 }
