90217c39de73d1a869261671d2df2139c124bae4
totk <totorkepic@hotmail.fr>
2015-06-05 11:29:24 +0200

    debug sliding, added slide and jump sound, sound refactoring wip

--- BubbleSlash/Assets/scripts/BoxController.cs
+++ BubbleSlash/Assets/scripts/BoxController.cs
@@ -1,15 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
 public class BoxController : MonoBehaviour {
 
 	public PlayerPhysics physics;
 	
 	void feetStay(){
-		physics.is_grounded = true;
+
 	}
 
 	void feetEnter(){
+		physics.is_grounded = true;
 	}
 
 	void feetExit(){
@@ -17,18 +19,21 @@ public class BoxController : MonoBehaviour {
 	}
 
 	void leftStay(){
-		physics.is_touching_left = true;
+
 	}
 	void leftEnter(){
+		physics.is_touching_left = true;
 	}
 	void leftExit(){
 		physics.is_touching_left = false;
 	}
 	void rightStay(){
-		physics.is_touching_right = true;
+
 	}
 	void rightEnter(){
+		physics.is_touching_right = true;
 	}
+
 	void rightExit(){
 		physics.is_touching_right = false;
 	}
