a937bc078c0c19f06f4b38a7855b160ffadd433a
totk <totorkepic@hotmail.fr>
2015-06-15 14:10:38 +0200

    better menu, with bubble preview

--- BubbleSlash/Assets/scripts/menuScript.cs
+++ BubbleSlash/Assets/scripts/menuScript.cs
@@ -52,24 +52,22 @@ public class menuScript : MonoBehaviour {
 	}
 
 	void addPlayer(){
-		GameObject new_pfm = GameObject.Instantiate(playerFactoryMenuPrefab) as GameObject;
-		RectTransform rect_tr = new_pfm.GetComponent<RectTransform> ();
-		rect_tr.SetParent (GameObject.Find ("PlayerFactoryMenus").GetComponent<RectTransform> ());
-		new_pfm.transform.localScale = new Vector3 (1, 1, 1);
-		//GameObject.Find ("PlayerFactoryMenus").GetComponent<RectTransform> ().offsetMin = new Vector2 (0, 0);
-
-		//new_pfm.GetComponent<RectTransform> ().position = new Vector3 (0, 500- 80*playerFactoryMenus.Count, 0);
-		//new_pfm.GetComponent<PlayerFactoryMenuScript> ().setInputNumber (playerFactoryMenus.Count + 1);
-		playerFactoryMenus.Add (new_pfm);
-		GameObject.Find ("PlayerFactoryMenus").GetComponent<RectTransform> ().offsetMin = new Vector2 (0, 80 + 90 *(4- playerFactoryMenus.Count));
-		//GameObject player_factory = GameObject.Instantiate(playerFactoryPrefab) as GameObject;
-		//online_game.addFactory (player_factory.GetComponent<PlayerFactory> ());
+		if (playerFactoryMenus.Count < 4) {
+			GameObject new_pfm = GameObject.Instantiate (playerFactoryMenuPrefab) as GameObject;
+			RectTransform rect_tr = new_pfm.GetComponent<RectTransform> ();
+			rect_tr.SetParent (GameObject.Find ("PlayerFactoryMenus").GetComponent<RectTransform> ());
+			new_pfm.transform.localScale = new Vector3 (1, 1, 1);
+			playerFactoryMenus.Add (new_pfm);
+			GameObject.Find ("PlayerFactoryMenus").GetComponent<RectTransform> ().offsetMin = new Vector2 (0, 80 + 90 * (4 - playerFactoryMenus.Count));
+		}
 	}
 	void removePlayer(){
-		GameObject pfm = playerFactoryMenus [playerFactoryMenus.Count - 1];
-		playerFactoryMenus.Remove (pfm);
-		GameObject.Destroy (pfm);
-		GameObject.Find ("PlayerFactoryMenus").GetComponent<RectTransform> ().offsetMin = new Vector2 (0, 80 + 90 *(4- playerFactoryMenus.Count));
+		if (playerFactoryMenus.Count > 0) {
+			GameObject pfm = playerFactoryMenus [playerFactoryMenus.Count - 1];
+			playerFactoryMenus.Remove (pfm);
+			GameObject.Destroy (pfm);
+			GameObject.Find ("PlayerFactoryMenus").GetComponent<RectTransform> ().offsetMin = new Vector2 (0, 80 + 90 * (4 - playerFactoryMenus.Count));
+		}
 
 	}
 
