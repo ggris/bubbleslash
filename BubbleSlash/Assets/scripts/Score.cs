86c1cd5b56dd8825c52f5879d3098e5a6d4e14d5
Guillaume Gris <guillaume.gris_code@m4x.org>
2015-06-16 11:21:37 +0200

    Added score and notifications

--- BubbleSlash/Assets/scripts/Score.cs
+++ BubbleSlash/Assets/scripts/Score.cs
@@ -5,12 +5,17 @@ using UnityEngine.UI;
 public class Score : MonoBehaviour {
 
 	public Text value_;
+	public GameObject message_prefab_;
+	static GameObject message_;
+	static GameObject scorePanel_;
 	static float score_ = 0;
 	static float power_ = 1;
 
 	// Use this for initialization
 	void Start () {
 		score_ = 0;
+		message_ = message_prefab_;
+		scorePanel_ = gameObject;
 	}
 	
 	// Update is called once per frame
@@ -21,22 +26,31 @@ public class Score : MonoBehaviour {
 	public static void kill() {
 		score_ += 42 * power_;
 		power_ *= 1.01f;
+		announce ("kill");
 	}
 
 	public static void hurt() {
 		score_ += 13 * power_;
 		power_ += 0.4f;
+		announce ("hurt");
 	}
 
 	public static void hit() {
 		score_ -= 17;
 		if (score_<0) score_ = 0;
 		power_ /= 2;
+		announce ("hit");
 	}
 
 	public static void die() {
 		score_ *= 0.92f;
 		power_ = 1;
+		announce ("you die");
+	}
+
+	public static void announce(string message = "yey") {
+		message_.GetComponent<Text> ().text = message;
+		GameObject.Instantiate (message_).transform.SetParent(scorePanel_.transform, false);
 	}
 
 }
---
+++ BubbleSlash/Assets/scripts/ScrollAndDie.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScrollAndDie : MonoBehaviour {
+
+	float t_;
+	public Color color_ = new Color (1, 0, 1, 1);
+
+	// Use this for initialization
+	void Start () {
+		t_ = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float x = Time.time - t_;
+		if (x <= 0.2)
+			GetComponent<RectTransform> ().localScale *= 1.1f;
+		else
+			GetComponent<RectTransform> ().localScale += new Vector3(1, 1, 1)*0.01f;
+		color_.a *= 0.95f;
+		GetComponent<Text> () .color = color_;
+	
+	}
+}
