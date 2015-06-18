f15636ba3a1f4a1069f0753e94812080788725b6
Guillaume Gris <guillaume.gris_code@m4x.org>
2015-06-18 14:54:59 +0200

    shockwave bug patch try

--- BubbleSlash/Assets/scripts/CameraPost/ShockWave.cs
+++ BubbleSlash/Assets/scripts/CameraPost/ShockWave.cs
@@ -8,25 +8,26 @@ public class ShockWave : MonoBehaviour
 	public float max_time_ = 1;
 	public float variance_ = 0.5f;
 	public float amplitude_ = 0.01f;
-
 	private Material internal_material_;
 	private Vector3 center_;
 	private float time_;
 
-	void Start () {
+	void Start ()
+	{
 		time_ = Time.time - max_time_; // No Shockwave on start.
 		internal_material_ = new Material (shockwave_material_);
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 		if (Input.GetKeyDown ("i")) {
 			time_ = Time.time;
 		}
 		
 	}
 
-	public void pop(Vector3 center)
+	public void pop (Vector3 center)
 	{
 		time_ = Time.time;
 		center_ = center;
@@ -35,27 +36,26 @@ public class ShockWave : MonoBehaviour
 
 	float getRadius (float delta_t)
 	{
-		float radius = delta_t * ( 0.2f*delta_t + 0.5f ) ;
+		float radius = delta_t * (0.2f * delta_t + 0.5f);
 		radius *= speed_;
-
 
 		return radius;
 	}
 
 	void OnRenderImage (RenderTexture source, RenderTexture destination)
 	{
-		float delta_t = 0.01f +Time.time - time_;
+		float delta_t = 0.01f + Time.time - time_;
 		if (delta_t < max_time_) {
 			float radius = getRadius (delta_t);
 			Vector3 center = GetComponent<Camera> ().WorldToViewportPoint (center_);
 			
 			float scale = -transform.position.z;
 			radius /= scale;
-			internal_material_.SetFloat ("_Radius", radius/scale);
+			internal_material_.SetFloat ("_Radius", radius / scale);
 			internal_material_.SetVector ("_Center", center);
-			internal_material_.SetFloat ("_Sigma", variance_/scale*radius);
-			internal_material_.SetFloat ("_Amplitude", amplitude_/scale*radius);
-			internal_material_.SetFloat ("_Ratio", GetComponent<Camera>().aspect);
+			internal_material_.SetFloat ("_Sigma", variance_ / scale * radius);
+			internal_material_.SetFloat ("_Amplitude", amplitude_ / scale * radius);
+			internal_material_.SetFloat ("_Ratio", GetComponent<Camera> ().aspect);
 			Graphics.Blit (source, destination, internal_material_);
 		} else {
 			Graphics.Blit (source, destination);
